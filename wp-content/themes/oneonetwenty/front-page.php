67930dc9ec4bd170c8046c1b1dc1b6065f2c79f6
tomas <tomascostapp@gmail.com>
2025-01-29 18:22:52 -0300

    Avances home

--- wp-content/themes/oneonetwenty/front-page.php
+++ wp-content/themes/oneonetwenty/front-page.php
@@ -2,10 +2,10 @@
 $hero_fields = get_field('hero');
 get_header(); 
 ?>
-<section class="relative w-full h-[700px]">
-	<video class="w-full" src="<?php echo $hero_fields['fondo']['url'] ?>" autoplay="true" muted="true" loop="true" poster=""></video>
+<section class="relative w-full h-[600px] flex items-end justify-center py-[80px]">
+	<video class="w-full h-full object-cover absolute top-0 left-0" src="<?php echo $hero_fields['fondo']['url'] ?>" autoplay="true" muted="true" loop="true" poster=""></video>
     <div style="position: relative; z-index: 1; color: white;">
-        <h1><?php echo get_the_title(); ?></h1>
+        <h1 class="text-white uppercase text-[24px] font-bold"><?php echo $hero_fields['titulo']; ?></h1>
     </div>
 </section>
 <?php get_footer(); ?>
