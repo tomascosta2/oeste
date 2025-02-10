ed3754e01f5723aa7dfe80f78883059f67d6fa09
tomas <tomascostapp@gmail.com>
2025-02-10 10:37:18 -0300

    Optimizo JS

--- wp-content/themes/oneonetwenty/footer.php
+++ wp-content/themes/oneonetwenty/footer.php
@@ -9,8 +9,8 @@
 <?php wp_footer(); ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/fff50d7907.js" crossorigin="anonymous" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js" defer></script>
+<!-- <script src="https://kit.fontawesome.com/fff50d7907.js" crossorigin="anonymous"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 <script>
 	const menuOpener = document.getElementById('menuOpener');
